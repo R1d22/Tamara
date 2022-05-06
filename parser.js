0b2db4fb79d0e997dcf98dd88c0b76c94504191b
37flwr <nesterishinyarik@gmail.com>
2022-05-06 21:00:17 +0300

    add title; add brand; add sizing

--- parser.js
+++ parser.js
@@ -3,9 +3,52 @@ const puppeteer = require("puppeteer");
 const fs = require("fs/promises");
 
 // Consts
-const washington = "90001";
-const link = "https://www.amazon.com/dp/B08DYHVF2K";
 const linkAppend = "https://www.amazon.com/dp/";
+const defaultSizing = [
+  {color: [
+  'Black',
+  'White',
+  'Blue',
+  'Green',
+  'Grey',
+  'Clear',
+  'Red',
+  'Purple',
+  'Brown',
+  'Multicolor',
+  'Aqua',
+  'Assorted',
+  ]},
+  {material: [
+    'Wood',
+    'Plastic',
+    'Rubber',
+    'Stone',
+    'Leather',
+    'Fur',
+    'Synthetic',
+  ]},
+  {size: [
+    'Small',
+    'Medium',
+    'Large',
+    '1 Pack',
+    '1Pack',
+    'inch',
+    'oz',
+    'count',
+  ]},
+  {style: [
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+  ]}
+];
 
 (async () => {
   // Iterate variables
@@ -16,41 +59,18 @@ const linkAppend = "https://www.amazon.com/dp/";
 
   // Puppeteer presets
   const browser = await puppeteer.launch({
-    headless: true,
+    headless: false,
     args: ["--no-sandbox", "--disable-dev-shm-usage"],
   });
   const page = await browser.newPage();
-  await page.setViewport({
-    width: 1200,
-    height: 800,
-  });
   await page.setDefaultNavigationTimeout(60000);
 
-  // Create files
-  await fs.writeFile("./result/Parents_no_variations.txt", "");
-  await fs.writeFile("./result/2PackBrands.txt", "");
-  await fs.writeFile("./result/No_Parents.txt", "");
-  await fs.writeFile("./result/Musor.txt", "");
+  // // Create files
   await fs.writeFile("./result/Rerun.txt", "");
-  await fs.writeFile("./result/OutOfStock.txt", "");
-
-  //  Swap location
-  await page.goto(link);
-  await page.waitForTimeout(4000);
-  await page.click('[id="nav-global-location-slot"]');
-  await page.waitForSelector("input.a-declarative");
-  await page.waitForTimeout(4000);
-  await page.type("input.a-declarative", washington);
-  await page.waitForTimeout(4000);
-  await page.click("#GLUXZipUpdate");
-  await page.waitForTimeout(4000);
-  await page.click(".a-button-input");
-  await page.waitForTimeout(4000);
 
   const checkLinks = async () => {
     // .readFile functions
     const rawLinks = await fs.readFile("./toCheck/links.txt", "utf-8");
-    const rawBrands = await fs.readFile("./toCheck/brands.txt", "utf-8");
     const checkForRerun = await fs.readFile("./result/Rerun.txt", "utf-8");
 
     // Functional lists
@@ -61,122 +81,41 @@ const linkAppend = "https://www.amazon.com/dp/";
       links = checkForRerun.split("\n");
       await fs.writeFile("./result/Rerun.txt", "");
     }
-    const brands = rawBrands.split(";");
 
-    // const asins = links.map((link) => {
-    //     const sliced = link.slice(26);
-    //     return sliced;
-    // });
+    // File variables 
+    let titleForFile;
+    let brandForFile;
+    let partNumberForFile;
+    let photosForFile;
+    let sizingForFile;
+    let productTypeForFile;
 
     // Product Check
     for (let i = 0; i < links.length; i++) {
+      let link;
+
+      if(links[i].length === 10) {
+        link = linkAppend + links[i]
+      } else {
+        link = links[i]
+      };
+
       // Redirection to product page
-      await page.goto(links[i], { waitUntil: "domcontentloaded" });
-      // await page.waitForTimeout(10000)
+      await page.goto(link, { waitUntil: "networkidle2" });
       const botCheck = await page.evaluate(() => {
         return document.querySelector(".a-padding-extra-large");
       });
 
-      // Check for BSR
-      //   const tableBsr = await page.evaluate(() => {
-      //     return Array.from(document.getElementsByTagName("tr")).map(
-      //       (x) => x.textContent
-      //     );
-      //   });
-
-      //   const tableBsrCheck = tableBsr.map((x) => {
-      //     if (x.indexOf("Best Sellers Rank") > -1) {
-      //       return x;
-      //     } else return undefined;
-      //   });
-
-      //   const nonTableBsr = await page.evaluate(() => {
-      //     return Array.from(document.querySelectorAll(".a-list-item")).map(
-      //       (x) => x.textContent
-      //     );
-      //   });
-
-      //   const nonTableBsrCheck = nonTableBsr.map((x) => {
-      //     if (x.indexOf("Best Sellers Rank") > -1) {
-      //       return x;
-      //     } else return undefined;
-      //   });
-
-      //   const resultTableBsr = tableBsrCheck.filter((element) => {
-      //     return element !== undefined;
-      //   });
-
-      //   const resultNonTableBsr = nonTableBsrCheck.filter((element) => {
-      //     return element !== undefined;
-      //   });
-
-      //   let result;
-
-      //   if (resultTableBsr[0]) {
-      //     result = resultTableBsr[0];
-      //   } else {
-      //     result = resultNonTableBsr[0];
-      //   }
-
-      //   const preprocessed = result
-      //     .replace("Best Sellers Rank", "")
-      //     .split("in")[0];
-      //   const processed = preprocessed
-      //     .substring(preprocessed.indexOf("#"))
-      //     .replace(" ", "")
-      //     .replace("#", "")
-      //     .replace(",", "");
-      //   console.log(processed);
-
-      //   const category = result
-      //     .replace("Best Sellers Rank", "")
-      //     .split("in")[1]
-      //     .split("(")[0]
-      //     .replace(" ", "");
-      //   console.log(category);
-
-      // Check for out of stock
-      const outOfStock = await page.evaluate(() => {
-        return document.querySelector("#outOfStock");
-      });
-
-      // Check for parents
-      const parents = await page.evaluate(() => {
-        return Array.from(
-          document.querySelectorAll(
-            "#twisterContainer > div > form div[class='a-section a-spacing-small']"
-          )
-        );
-      });
-
-      await page.waitForTimeout(500);
-
-      // Check for variations if there is a parent
-      let variations = 0;
-      if (parents.length > 0) {
-        const variationsUsual = await page.evaluate(() => {
-          return Array.from(document.querySelectorAll(".swatches"));
-        });
-        const variationsDropDown = await page.evaluate(() => {
-          return Array.from(document.querySelectorAll(".aui-variation"));
-        });
-        const fakeVariation = await page.evaluate(() => {
-          return document.querySelector(".vas-ppd-swatch-select");
-        });
-        if (fakeVariation) {
-          variations = 0;
-        }
-        if (variationsUsual.length > 0 || variationsDropDown.length > 0) {
-          variations = 1;
-        }
+      if(botCheck) {
+        continue
       }
+
+      await page.waitForTimeout(1000)
 
       // Get brand name
       const brandName = await page.evaluate(() => {
         try {
-          const { textContent, innerText } =
-            document.getElementById("bylineInfo");
-          return textContent || innerText;
+          return document.querySelector("#bylineInfo").textContent;
         } catch {
           return `No brand was defined`;
         }
@@ -192,77 +131,78 @@ const linkAppend = "https://www.amazon.com/dp/";
           return `String doesn't include any check words`;
         }
       }
-      const checkedAndSliced = checkAndSlice(brandName);
+      brandForFile = checkAndSlice(brandName);
 
-      // Check for 2PackBrand
-      let twoPackBrand = 0;
-      const checkForTwoPackBrand = async () => {
-        for (let i = 0; i < brands.length; i++) {
-          if (brands[i] == checkedAndSliced) {
-            twoPackBrand = 1;
-          }
+      // Page evaluate
+      const pageValues = await page.evaluate(() => {
+        const title = document.querySelector('#productTitle').textContent;
+        const sizingParentNoVarsFirst = Array.from(document.querySelectorAll('.a-cardui-body > div > div > span')).map((x) => x.textContent)
+        const sizingParentNoVarsSecond = Array.from(document.querySelectorAll('#twister > div > div')).map((x) => x.textContent)
+        const sizingParentFirst = Array.from(document.querySelectorAll('.inline-twister-dim-title-value-truncate-expanded > span')).map((x) => x.textContent)
+        const sizingParentSecond = Array.from(document.querySelectorAll('#twister > div > div')).map((x) => x.textContent)
+        const partNumberLi = Array.from(document.querySelectorAll('#detailBullets_feature_div > ul > li > .a-list-item > span ')).map((x) => x.textContent)
+        const partNumberTableTitle = Array.from(document.querySelectorAll('#productDetails_detailBullets_sections1 > tbody > tr > th')).map((x) => x.textContent)
+        const partNumberTableData = Array.from(document.querySelectorAll('#productDetails_detailBullets_sections1 > tbody > tr > td')).map((x) => x.textContent)
+        const productOverview = Array.from(document.querySelectorAll('#productOverview_feature_div > div > table > tbody > tr > td')).map((x) => x.textContent)
+        return {title, sizingParentNoVarsFirst, sizingParentNoVarsSecond, sizingParentFirst, sizingParentSecond, partNumberLi, partNumberTableTitle, partNumberTableData, productOverview}
+      })
+
+      // Generate title for file
+      const titleForFileFunc = () => {
+        const includesTitle = pageValues.title.toLowerCase().includes(brandForFile.toLowerCase())
+        const indexOfBrand = pageValues.title.toLowerCase().indexOf(brandForFile.toLowerCase())
+        
+        if(includesTitle && indexOfBrand === 8) {
+          titleForFile = pageValues.title.slice(pageValues.title.toLowerCase().indexOf(brandForFile.toLowerCase())+brandForFile.length+1);
+        } else {
+          titleForFile = pageValues.title.slice(8);
         }
-      };
-      checkForTwoPackBrand();
+      }
 
-      // Check for sellers
-      const ifSellersPresent = await page.evaluate(() => {
-        return document.querySelector(".olp-touch-link");
-      });
-
-      let sellerMatch = 0;
-      if (ifSellersPresent) {
-        await page.click("a.olp-touch-link");
-        await page.waitForTimeout(4000);
-        const sellers = await page.evaluate(() => {
-          try {
-            return Array.from(document.querySelectorAll(".a-size-small")).map(
-              (x) => x.textContent
-            );
-          } catch {
-            console.log("Seller Error");
-          }
-        });
-        if (sellers.length > 0) {
-          let sellersArray = [];
-          for (let i = 0; i < sellers.length; i++) {
-            if (sellers[i].split(" ").join("") == "Soldby") {
-              sellerIndex = i++;
-              sellersArray.push(sellers[i++]);
+      // Generate part number for file
+      const partNumberForFileFunc = () => {
+        if(pageValues.partNumberTableTitle.length > 0) {
+          for(x=0;x<pageValues.partNumberTableTitle.length;x++) {
+            if(pageValues.partNumberTableTitle[x].replaceAll(' ', '') == 'Itemmodelnumber' || pageValues.partNumberTableTitle[x].replaceAll(' ', '') == 'PartNumber') {
+              partNumberForFile = pageValues.partNumberTableData[x].replace(' ', '')
+              break
             }
           }
-          if (sellersArray.length > 0) {
-            for (let i = 0; i < sellersArray.length; i++) {
-              if (
-                sellersArray[i].split(" ").join("") ==
-                checkedAndSliced.split(" ").join("")
-              ) {
-                sellerMatch = 1;
-              }
+        } else {
+          for(y=0;y<pageValues.partNumberLi.length;y++) {
+            if(pageValues.partNumberLi[y].replaceAll(/[^a-zA-Z ]/g, '').replaceAll(' ', '') == 'Itemmodelnumber' || pageValues.partNumberLi[y].replaceAll(/[^a-zA-Z ]/g, '').replaceAll(' ', '') == 'PartNumber') {
+              partNumberForFile = pageValues.partNumberLi[y+1]
+              break
             }
           }
         }
       }
 
-      // Fill in the corresponding file
+      // Generate sizing for files
+      // const sizingForFile = () => {
+      //   if 
+      // }
+
+      // Prepare variables for files
+      const prepareForFile = () => {
+        titleForFileFunc()
+        partNumberForFileFunc()
+      }
+
+      prepareForFile()
+
+      console.log(titleForFile);
+      console.log(brandForFile);
+      console.log(partNumberForFile);
+      console.log(pageValues.sizingParentNoVarsFirst);
+      console.log(pageValues.sizingParentNoVarsSecond);
+      console.log(pageValues.sizingParentFirst);
+      console.log(pageValues.sizingParentSecond);
+      console.log(pageValues.productOverview);
+      
+
       if (botCheck) {
-        await fs.appendFile("./result/Rerun.txt", links[i] + "\n");
-      } else if (sellerMatch || variations > 0) {
-        await fs.appendFile("./result/Musor.txt", links[i] + "\n");
-      } else if (twoPackBrand == 1 && variations == 0 && !outOfStock) {
-        await fs.appendFile("./result/2PackBrands.txt", links[i] + "\n");
-      } else if (parents.length > 0 && variations == 0 && !outOfStock) {
-        await fs.appendFile(
-          "./result/Parents_no_variations.txt",
-          links[i] + "\n"
-        );
-      } else if (parents.length == 0 && !outOfStock) {
-        await fs.appendFile("./result/No_Parents.txt", links[i] + "\n");
-      } else if (outOfStock) {
-        await fs.appendFile("./result/OutOfStock.txt", links[i] + "\n");
-      } else {
-        await fs.appendFile("./result/Musor.txt", links[i] + "\n");
-      }
+      await fs.appendFile("./result/Rerun.txt", links[i] + "\n");}
 
       number++;
       console.log(number);
